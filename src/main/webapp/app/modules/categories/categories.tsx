d6f605f32be739c293936241a710e85574b64fbc
mrmey13 <65488767+mrmey13@users.noreply.github.com>
2021-12-08 09:42:07 +0700

    update product card

--- src/main/webapp/app/modules/categories/categories.tsx
+++ src/main/webapp/app/modules/categories/categories.tsx
@@ -20,8 +20,32 @@ const Category = () => {
           <div className="product-list-card-header">
             <div className="tabs-link active">Product</div>
           </div>
-          <div className="product-list-card-body"></div>
-          <Product />
+          <div className="product-list-card-body">
+            <Product
+              product={{
+                id: 0,
+                name: 'Ten san pham',
+                mainPrice: 100000,
+                discountPrice: 2000,
+                url: '/category',
+                image: 'content/images/logo-jhipster.png',
+                rating: 1.5,
+                sold: 120,
+              }}
+            />
+            <Product
+              product={{
+                id: 0,
+                name: 'Ten san phamasd hgdasvg hdasfdasfdc',
+                mainPrice: 100000,
+                discountPrice: 2000,
+                url: '/category',
+                image: 'content/images/logo-jhipster.png',
+                rating: 1.5,
+                sold: 120,
+              }}
+            />
+          </div>
         </div>
       </div>
     </>
--- src/main/webapp/app/modules/categories/product.tsx
+++ src/main/webapp/app/modules/categories/product.tsx
@@ -3,22 +3,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Rate } from 'antd';
 
-const Product = () => {
+export type Product = {
+  id: number;
+  name: string;
+  image: string;
+  url: string;
+  mainPrice: number;
+  discountPrice?: number;
+  rating: number;
+  sold?: number;
+};
+
+const Product = ({ product }: { product: Product }) => {
+  const formatPrice = price => {
+    return price.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+  };
+
   return (
     <div className="product-card">
-      <div className="product-image">asdas</div>
-      <div className="product-name">
-        <Link to="/category">Ten san pham vippro cuc chat</Link>
+      <div className="product-image">
+        <img src={product.image} alt="Logo" />
       </div>
-      <div className="product-price">
-        <span className="discount-price">100000đ</span>
-        <span className="main-price">200000đ</span>
-      </div>
-      <div className="product-selled">
-        <span className="product-rate">
-          <Rate allowHalf value={3.6} disabled />
-        </span>
-        <span className="product-sold">Đã bán: 150</span>
+      <div className="product-card-body">
+        <div className="product-name">
+          <Link to={product.url}>{product.name}</Link>
+        </div>
+        <div className="product-price">
+          {product.discountPrice && <span className="discount-price">{`${formatPrice(product.discountPrice)}đ`}</span>}
+          <span className="main-price">{`${formatPrice(1000)}đ`}</span>
+        </div>
+        <div className="product-selled">
+          <span className="product-rate">
+            <Rate allowHalf value={product.rating} disabled />
+          </span>
+          {product.sold && <span className="product-sold">Đã bán: {product.sold}</span>}
+        </div>
       </div>
     </div>
   );
